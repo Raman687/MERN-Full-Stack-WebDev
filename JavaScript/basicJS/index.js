ef9359d4daa4f6a3249c17d170198bae6d9b888d
Raman687 <meenaraman687@gmail.com>
2025-11-16 18:55:45 +0530

    Basic Java Script

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,19 @@
+var a = 10;
+var a = 20;
+let b = 30;
+
+console.log(a, b);
+
+let ar = ["Abc", 123, 123.12, "a", true];
+console.log(ar);
+
+console.warn("its a Warning")
+console.error("its an Error")
+
+alert("its an Alert!");
+
+if(confirm("Are you Sure?")){
+    console.log("thanks")
+} else{
+    console.error("its ok")
+}
