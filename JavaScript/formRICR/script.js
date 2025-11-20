8431eb8b564271d551bb672fa54c19c4ceb77e11
Raman687 <meenaraman687@gmail.com>
2025-11-20 16:12:48 +0530

    RICR Enquiry Form

---
+++ JavaScript/formRICR/script.js
@@ -0,0 +1,35 @@
+function submitForm() {
+  console.log("Form Submitted");
+
+  const name = document.getElementById("personName").value;
+  const phone = document.getElementById("contactNumber").value;
+  const email = document.getElementById("email").value;
+  const qual = document.getElementById("qualification").value;
+  const college = document.getElementById("collegeSchool").value;
+  const year = document.getElementById("year").value;
+  const branch = document.getElementById("branch").value;
+  const source = document.getElementById("source").value;
+  const execName = document.getElementById("executive").value;
+
+  console.log("Name:", name);
+  console.log("Phone:", phone);
+  console.log("Email:", email);
+  console.log("Qualification:", qual);
+  console.log("College/School:", college);
+  console.log("Year:", year);
+  console.log("Branch:", branch);
+  console.log("Source:", source);
+  console.log("Executive:", execName);
+
+  alert("Form submitted successfully!");
+
+    document.getElementById("personName").value = "";
+    document.getElementById("contactNumber").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("qualification").value = "";
+    document.getElementById("collegeSchool").value = "";
+    document.getElementById("year").value = "";
+    document.getElementById("branch").value = "";
+    document.getElementById("source").value = "";
+    document.getElementById("executive").value = "";
+}
