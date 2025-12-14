ed4ff14905fbb1b3119a3efd76c1f0529d183835
Raman687 <meenaraman687@gmail.com>
2025-12-14 15:18:08 +0530

    Age Calculator

--- JokesApp/script.js
+++
@@ -1,9 +0,0 @@
-async function GetNewJoke(){
-
-    const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
-
-    const data = await response.json();
-    
-    document.getElementById("setup").innerText = data.setup;
-    document.getElementById("punchline").innerText = data.punchline;
-}
